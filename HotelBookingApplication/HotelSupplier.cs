29b6b8b4c68ea17b73e67c01d6b1e2ded9cff52f
karth707 <kartheek.nallepalli@gmail.com>
2014-09-28 22:50:20 -0700

    HotelSupplier comments added

--- HotelBookingApplication/HotelSupplier.cs
+++ HotelBookingApplication/HotelSupplier.cs
@@ -15,37 +15,37 @@ namespace HotelBookingApplication
         public static event priceChangeEvent priceChange;       //Event when regular price change 
         private Int32 roomPrice;
         private Int32 p;                                        //priceCut counter; if p=10, thread terminates                             
-       
+
         public HotelSupplier()
         {
             roomPrice = 10;
             p = 0;
         }
 
-        public Int32 getPrice() {                       //returns price
-            return roomPrice; 
+        public Int32 getPrice()
+        {                           //returns price
+            return roomPrice;
         }
 
-        public void changePrice(Int32 price)            //Changes the roomPrice to new price 
+        public void changePrice(Int32 price)                //Changes the roomPrice to new price 
         {
             if (price < roomPrice)
             {
-                p++;                                     //Increments p if there is a priceCut
-                if (priceCut != null)  
+                p++;                                        //Increments p if there is a priceCut
+                if (priceCut != null)
                     priceCut(price);
-                //Console.WriteLine("PriceCutCounter for Hotel:{0} is: {1}", Thread.CurrentThread.Name, p);
             }
             else
             {
                 if (priceChange != null)
                     priceChange(price);
-            }            
+            }
             roomPrice = price;
-            
+
         }
 
-        public void HotelFunc()                         //Thread Start function.
-        {                                               //Changes the price of the room for this hotel                    
+        public void HotelFunc()                             //Thread Start function.
+        {                                                   //Changes the price of the room for this hotel                    
             for (Int32 i = 0; i < 50; i++)
             {
                 if (p > 10)
@@ -55,12 +55,16 @@ namespace HotelBookingApplication
                     Thread.CurrentThread.Abort();
                 }
                 else
-                {                          
+                {
                     Int32 newPrice = rng.Next(5, 10);
-                    changePrice(newPrice);                    
-                    //Console.WriteLine("New price for Hotel:{0} is:{1}", Thread.CurrentThread.Name, newPrice);
+                    changePrice(newPrice);
+                    Console.WriteLine("New price for Hotel:{0} is:{1}", Thread.CurrentThread.Name, newPrice);
                     Thread.Sleep(500);
-                }                
+
+                    //check buffer for orders
+                    //decode orders
+                    //spawn orderProcessing threads for processing order
+                }
             }
         }
     }
