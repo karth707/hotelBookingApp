1b7b4f35ff2901ff59ccf83652cdc449fce2a043
karth707 <kartheek.nallepalli@gmail.com>
2014-10-04 15:33:22 -0700

    added buffer object

---
+++ HotelBookingApplication/BufferObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBookingApplication
+{
+    class BufferObject
+    {
+        private String hotel;
+        private String encodedOrder;
+
+        public BufferObject(String encodedOrder, String hotel)
+        {
+            this.encodedOrder = encodedOrder;
+            this.hotel = hotel;
+        }
+
+        public String getHotel()
+        {
+            return hotel;
+        }
+
+        public String getEncodedOrder()
+        {
+            return encodedOrder;
+        }
+    }
+}
